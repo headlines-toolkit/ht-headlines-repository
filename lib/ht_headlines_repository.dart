b1beb9e60791f1982599c6dba22107e55211a63e
fulleni <fulleni@hotmail.com>
2025-03-27 10:50:30 +0100

    chore: remove client export
    - Remove direct client export
    - Use repository implementation

--- lib/ht_headlines_repository.dart
+++ lib/ht_headlines_repository.dart
@@ -3,16 +3,4 @@
 /// searching headlines.
 library;
 
-export 'package:ht_headlines_client/ht_headlines_client.dart'
-    show
-        Headline,
-        HeadlineCreateException,
-        HeadlineDeleteException,
-        HeadlineNotFoundException,
-        HeadlineUpdateException,
-        HeadlinesException,
-        HeadlinesFetchException,
-        HeadlinesSearchException,
-        HtHeadlinesClient;
-
 export 'src/ht_headlines_repository.dart';
